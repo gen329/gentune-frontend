bee64c5b13e4aad8610f02b658acb7ecabdf0ac6
Genesis Lara <genesislara@pursuit.org>
2023-11-20 12:43:02 -0500

    adds navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,1 +1,17 @@
-export default Navbar;
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <header>
+        GenTune
+      </header>
+      <button>
+        <Link to="/songs">Song Library</Link>
+      </button>
+      <button>
+        Request a Song!
+      </button>
+    </nav>
+  );
+}
