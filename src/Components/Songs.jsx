68a1fe2da0a6ff62e64cb2b480afa087021ffded
Genesis Lara <genesislara@pursuit.org>
2023-11-20 00:03:06 -0500

    works on songs file

---
+++ src/Components/Songs.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import Songs from "./Song";
+
+const API = import.meta.env.VITE_API_URL;
+
+function Songs() {
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/songs`)
+    .then((response) => response.json())
+    .then((data) => setSongs(data.data.payload))
+    .catch((error) => console.error(error, "Did not fetch movies"));
+  }, []);
+
+  return (
+    <div className="songs">
+      <section>
+        <table>
+          <thead>
+          </thead>
+          <tbody>
+            {songs.map((song) => (
+              <Song key={song.id} song={song} />
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+}
+
+export default Songs;
