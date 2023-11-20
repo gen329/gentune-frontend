91b5ff3c3a70e489798f1c81a4f58b6cb06ece60
Genesis Lara <genesislara@pursuit.org>
2023-11-20 13:16:54 -0500

    works on song details

--- src/Components/SongDetails.jsx
+++ src/Components/SongDetails.jsx
@@ -1,1 +1,62 @@
+import { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+const API = import.meta.env.VITE_API_URL;
+
+function SongDetails() {
+  const { id } = useParams();
+  const [song, setSong] = useState(null);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchSongDetails = async () => {
+      try {
+        const response = await fetch(`${API}/songs/${id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch song detials');
+        }
+        const data = await response.json();
+        setSong(data);
+      } catch (error) {
+        console.error('Fetch error', error);
+        setError('Failed to fetch data. Please try again.')
+      }
+    };
+    fetchSongDetails();
+  }, [id]);
+
+  const handleDelete = () => {
+    const httpOptions = { method: "DELETE" }
+    fetch(`${API}/songs/${id}`, httpOptions)
+      .then(() => navigate(`/songs`))
+      .catch(error => console.log(error))
+  }
+
+  if (!song) {
+    return <div>LOADING...</div>;
+  }
+
+  return (
+    <article>
+      <h1>SONG DETAILS</h1>
+      <h2>{song.title}</h2>
+      <p>Artist: {song.artist}</p>
+      <p>Album: {song.album}</p>
+      <p>Year: {song.year}</p>
+      <div>
+        <button className='back-button'>Back
+          <Link to={`/songs`}>Song Library</Link>
+        </button>
+        <br />
+        <button className='edit-button'>Edit
+          <Link to={`/songs/${id}/edit`}>Edit Song Info</Link>
+        </button>
+        <br />
+        <button onClick={handleDelete}>DELETE SONG</button>
+      </div>
+    </article>
+  );
+}
+
 export default SongDetails;
