73c59727e6ca029e9ef495b742220a236c65d80a
Genesis Lara <genesislara@pursuit.org>
2023-11-20 15:08:45 -0500

    works on new song form

--- src/Components/SongNewForm.jsx
+++ src/Components/SongNewForm.jsx
@@ -1,1 +1,52 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const API = import.meta.env.VITE_API_URL
+
+function SongNewForm() {
+  const navigate = useNavigate();
+  const [song, setSong] = useState({
+    title: "",
+    artist_name: "",
+    album: "",
+    genre: [],
+    year_of_release: 0,
+    is_favorite: false,
+  });
+
+  const addSong = () => {
+    fetch(`${API}/songs`, {
+      method: "POST",
+      body: JSON.stringify(song),
+      headers: {
+        "Content-Type": "application/song",
+      },
+    })
+      .then(() => {
+        navigate(`/songs`);
+      })
+      .catch((error) => console.error("catch", error));
+  };
+
+  const handleTextChange = (event) => {
+    event.preventDefault();
+    addSong();
+  };
+
+  return (
+    <div className='newSong'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Song Title:</label>
+        <input
+          id="title"
+          value={song.title}
+          type="text"
+          onChange={handleTextChange}
+          required
+        />
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
 export default SongNewForm;
