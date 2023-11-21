f4fa8fc350619ac5692cdaedd84c3cd3415dfe1e
Genesis Lara <genesislara@pursuit.org>
2023-11-20 19:53:05 -0500

    adds searchbar

--- src/Components/Searchbar.jsx
+++ src/Components/Searchbar.jsx
@@ -1,2 +1,58 @@
 import { useState } from 'react';
-import { getSearchResults }
+import { getSearchResults } from '../common/helper';
+
+const Searchbar = ({ updateResultState }) => {
+  const [searchQuery, updateSearchQuery] = useState('')
+  const [errorFound, updateErrorFound] = useState({
+    isError: false,
+    errorCode: 0
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (updateResultState) {
+      getSearchResults(searchQuery)
+        .then((response) => {
+          if (typeof response != "number") {
+            updateResultState(response)
+          } else {
+            updateErrorFound({
+              isError: true,
+              errorCode: response
+            })
+          }
+        })
+    } else {
+      console.error("Result state not valid")
+    }
+  }
+
+  const handleTextChange = (event) => {
+    updateSearchQuery(e.target.value)
+  }
+
+  return (
+    <div className='searchbar'>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleTextChange}
+          placeholder='search'
+          required />
+          <input type='submit' value="🔎"/>
+      </form>
+      <br />
+        {errorFound.isError ? (
+          <>
+          <div className='error'>
+            <h1>An error has occured!</h1>
+            <h2>Error Code: {errorFound.errorCode}</h2>
+          </div>
+          </>
+        ): null}
+    </div>
+  );
+}
+
+export default Searchbar;
---
+++ src/common/helper.js
@@ -0,0 +1,28 @@
+const API = import.meta.env.VITE_API_URL
+
+const getSong = () => {
+
+const getSearchResults = (query) => {
+  return fetch(`${API}/songs`)
+  .then(res => {
+    let code = res.status
+    if(code == 200){
+      return res.json()
+    } else {
+      return code
+    }
+  })
+  .then(json => {
+    if(typeof json != "number") {
+      return json.items
+    } else {
+      return json
+    }
+  })
+  .catch(error => console.error(error))
+}
+
+export {
+  getSong,
+  getSearchResults
+}
