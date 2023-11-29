5ef493e9f56b85b120b11fde6cc366de9dc7a261
Genesis Lara <genesislara@pursuit.org>
2023-11-29 12:28:20 -0500

    adds welcome message

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,16 @@ function Home() {
 
   return (
     <div className='flex'>
-      <h2>Welcome</h2>
+      <h2>Welcome to GenTune - Your Music Library Solution!
+
+GenTune is your go-to destination for organizing and enjoying your music collection hassle-free. It's a user-friendly platform designed to help you explore a wide array of songs, albums, artists, and playlists effortlessly.
+
+Discover new tracks, dive into albums, and create playlists that suit your taste. Our platform offers easy navigation, allowing you to find and manage your favorite music without any fuss.
+
+Whether you're looking for specific songs, want to organize your music, or simply explore different genres, GenTune has got you covered.
+Start your musical journey with GenTune and experience the joy of seamless music management and discovery!
+Enjoy your tunes! 🎶
+</h2>
     </div>
   );
 }
