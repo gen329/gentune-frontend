a39ce6a276dd85e700fc13baa4e3fcdfc3ff36ad
Genesis Lara <genesislara@pursuit.org>
2023-11-29 19:27:16 -0500

    saves changes

--- src/Components/SongEditForm.jsx
+++ src/Components/SongEditForm.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
-
+const validGenres = ["R&B", "Hip-Hop", "Pop", "Salsa", "Urbano Latino", "Afrobeats", "Hip-Hop/Rap"];
 const API = import.meta.env.VITE_API_URL;
 
 function SongEditForm() {
   let { id } = useParams();
   const navigate = useNavigate();
+  const [selectedGenre, setSelectedGenre] = useState('');
 
   const [song, setSong] = useState({
     title: "",
@@ -20,25 +21,25 @@ function SongEditForm() {
     setSong({ ...song, [event.target.id]: event.target.value });
   };
 
-    useEffect(() => {
-      fetch(`${API}/songs/${id}`)
-        .then(response => response.json())
-        .then(fetchedSong => {
-          setSong(fetchedSong)
-        })
-        .catch(() => navigate("/not-found"))
-    }, [id, navigate]);
+  useEffect(() => {
+    fetch(`${API}/songs/${id}`)
+      .then(response => response.json())
+      .then(fetchedSong => {
+        setSong(fetchedSong)
+      })
+      .catch(() => navigate("/not-found"))
+  }, [id, navigate]);
 
   const updateSong = () => {
     const httpOptions = {
-      "method" : "PUT",
-      "body" : JSON.stringify(song),
-      "headers" : {
-        "Content-Type" : "application/json"
+      method: "PUT",
+      body: JSON.stringify(song),
+      headers: {
+        "Content-Type": "application/json"
+      }
     }
-  }
 
-    fetch(`${API}/songs/${id}`, httpOptions) 
+    fetch(`${API}/songs/${id}`, httpOptions)
       .then(() => {
         alert(`${song.title} has been updated!`);
         navigate(`/songs`)
@@ -49,6 +50,10 @@ function SongEditForm() {
   const handleSubmit = (event) => {
     event.preventDefault();
     updateSong();
+  };
+
+  const handleGenreChange = (event) => {
+    setSelectedGenre(event.target.value);
   };
 
   return (
@@ -63,7 +68,7 @@ function SongEditForm() {
         />
         <label htmlFor="artist_name">Artist Name:</label>
         <input
-          id="name"
+          id="artist_name"
           value={song.artist_name}
           type="text"
           onChange={handleTextChange}
@@ -75,23 +80,35 @@ function SongEditForm() {
           type="text"
           onChange={handleTextChange}
         />
-        <label htmlFor="year">Year of Release:</label>
-        <input
-          id="yearOfRelease"
-          value={song.year_of_realease}
-          type="number"
-          onChange={handleTextChange}
-        />
         <label htmlFor="genre">Song Genre:</label>
         <input
           id="genre"
           value={song.genre}
           type="text"
           onChange={handleTextChange}
-          />
-       
-          <input className='border border-black rounded-sm px-8 py-2 mb-2' type="submit" onClick={handleSubmit} />
+        />
+        <label htmlFor="year">Year of Release:</label>
+        <input
+          id="yearOfRelease"
+          value={song.year_of_release}
+          type="number"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="genre">Select Genre:</label>
+        <select 
+        id="genre" 
+        value={selectedGenre} 
+        onChange={handleGenreChange}>
+          <option value="">Select a genre</option>
+          {validGenres.map((genre, index) => (
+            <option key={index} value={genre}>
+              {genre}
+            </option>
+          ))}
+        </select>
+        {selectedGenre && <p>Selected genre: {selectedGenre}</p>}
 
+        <input className='border border-black rounded-sm px-8 py-2 mb-2' type="submit" onClick={handleSubmit} />
       </form>
       <Link to={`/songs/${id}`}>
         <button className='border border-black rounded-sm px-8 py-2 mb-2'>GO BACK!</button>
